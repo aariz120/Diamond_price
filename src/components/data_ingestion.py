b4f0d3d5291b278da31d4ae3888bbc155f6e8403
aariz120 <aariz120@gmail.com>
2023-04-16 18:31:52 +0530

    Ingestionandtransformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,4 +1,4 @@
-import os 
+import os
 import sys
 from src.loggers import logging
 from src.exception import CustomException
@@ -10,22 +10,22 @@ from src.components.data_transformation import DataTransformation
 
 
 ## Intitialize the Data Ingetion Configuration
+
 @dataclass
 class DataIngestionconfig:
     train_data_path:str=os.path.join('artifacts','train.csv')
     test_data_path:str=os.path.join('artifacts','test.csv')
     raw_data_path:str=os.path.join('artifacts','raw.csv')
 
-
 ## create a class for Data Ingestion
 class DataIngestion:
     def __init__(self):
         self.ingestion_config=DataIngestionconfig()
 
-def initiate_data_ingestion(self):
+    def initiate_data_ingestion(self):
         logging.info('Data Ingestion methods Starts')
         try:
-            df=pd.read_csv(os.path.join('notebooks/data','mygemstonedata.csv'))
+            df=pd.read_csv(os.path.join('notebooks/','mygemstonedata.csv'))
             logging.info('Dataset read as pandas Dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
@@ -47,3 +47,10 @@ def initiate_data_ingestion(self):
         except Exception as e:
             logging.info('Exception occured at Data Ingestion stage')
             raise CustomException(e,sys)
+        
+
+
+        # run Data Ingestion
+       
+
+       
